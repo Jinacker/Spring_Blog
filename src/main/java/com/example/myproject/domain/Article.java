abd4eb987ae2cb63c1bfabf8727838c846156c03
Jinacker <rlawls1448@naver.com>
2025-06-27 11:00:05 +0900

    블로그 글 View 구현

--- src/main/java/com/example/myproject/domain/Article.java
+++ src/main/java/com/example/myproject/domain/Article.java
@@ -2,6 +2,10 @@ package com.example.myproject.domain;
 
 import jakarta.persistence.*;       // JPA
 import lombok.*;                   // Lombok (Getter, Builder, NoArgsConstructor, AccessLevel)
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Entity                    // 엔티티로 지정
 @Getter                    // 모든 필드 getter
@@ -29,4 +33,12 @@ public class Article {
         this.title = title;
         this.content = content;
     }
+
+    @CreatedDate // 엔티티가 생성될 때 생성 시간 저장
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate // 엔티티가 수정될 때 수정 시간 저장
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
